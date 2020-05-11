e1882e33caaaa0def40043491601e44892b82df1
sasz94 <sacha.bartlomiej@gmail.com>
2020-05-12 00:01:05 +0200

    rearange source code to separate functions

--- index.js
+++ index.js
@@ -21,6 +21,13 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
         const direction = event.wheelDelta < 0 ? 1 : -1;
+        scroll(direction);
+
+    })
+
+
+
+    function scroll(direction) {
         if (direction === 1) {
             const isLastSection = currentSectionIndex === sections.length - 1;
             if (isLastSection) return;
@@ -33,13 +40,20 @@ document.addEventListener('DOMContentLoaded', function () {
 
         currentSectionIndex = currentSectionIndex + direction;
 
-        console.log(currentSectionIndex);
 
+        scrollToCurrentSection();
+
+    }
+
+
+    function scrollToCurrentSection() {
 
         sections[currentSectionIndex].scrollIntoView({
             behavior: 'smooth',
             block: 'start'
         })
+    }
 
-    })
+
+
 })
